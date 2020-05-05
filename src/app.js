fb63cba8f506aacb97b2df543602d9e55f939d24
Baldecchi <tbaldecchi@C02XD70CJHD2.local>
2020-05-05 08:21:57 -0400

    Setup App for Horku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Way to get the nodemon to work correctly nodemon src/app.js -e js,hbs
 
@@ -102,6 +103,6 @@ app.get('*',(req, res)=>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 });
