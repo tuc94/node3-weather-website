a8a4078d9929954515b18e8b31fea684f56d558f
Baldecchi <tbaldecchi@C02XD70CJHD2.local>
2020-05-05 09:42:26 -0400

    added daily tempature functionality

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude,callback) =>{
         } else if (body.error) {
             callback('unable to find location',undefined)
         } else{
-            callback(undefined,body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.")
+            callback(undefined,body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain." +" The Tempature high today will be "+body.daily.data[0].temperatureMax+" degrees. The tempature low for the day will be " +body.daily.data[0].temperatureMin + " degress.")
         }
     })
 }
